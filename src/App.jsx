14e44bf52b714858530b6c52f77d4dde2de5460c
Rabss <rabssdev@gmail.com>
2024-05-10 19:28:13 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='text-red-500'>Hello Andrianina</h1>
+      <h1 className="text-red-500">Hello Andrianina</h1>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
