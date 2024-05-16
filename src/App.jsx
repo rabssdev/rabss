8423f636832c10f1d0e660b01243dd2273eaf97e
Rabss <rabssdev@gmail.com>
2024-05-16 17:01:34 +0300

    commit ajout 3D

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { Menu } from "./components/Menu";
 import { ScrollManager } from "./components/ScrollManager";
 import { framerMotionConfig } from "./config";
 
+
 function App() {
   const [section, setSection] = useState(0);
   const [menuOpened, setMenuOpened] = useState(false);
